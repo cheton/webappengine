744338f5931c018b2090f3adcda6f12e62917efa
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-08-10 02:34:56 -0700

    Add supported languages

--- web/components/header/index.jsx
+++ web/components/header/index.jsx
@@ -20,6 +20,22 @@ var Header = React.createClass({
                     <div className="navbar-collapse collapse" id="navbar-collapse">
                         <ul className="nav navbar-nav">
                             <li><Link to="dashboard">Dashboard</Link></li>
+                            <li className="dropdown">
+                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button">Settings <span className="caret"></span></a>
+                                <ul className="dropdown-menu" role="menu">
+                                    <li><a  href="#">Help</a></li>
+                                    <li role="separator" className="divider"></li>
+                                    <li className="dropdown-header">Language</li>
+                                    <li><a href="?lang=de">Deutsch</a></li>
+                                    <li><a href="?lang=en">English (US)</a></li>
+                                    <li><a href="?lang=es">Español</a></li>
+                                    <li><a href="?lang=fr">Français</a></li>
+                                    <li><a href="?lang=it">Italiano</a></li>
+                                    <li><a href="?lang=ja">日本語</a></li>
+                                    <li><a href="?lang=zh-cn">中文 (简体)</a></li>
+                                    <li><a href="?lang=zh-tw">中文 (繁體)</a></li>
+                                </ul>
+                            </li>
                         </ul>
                     </div>
                 </div>
