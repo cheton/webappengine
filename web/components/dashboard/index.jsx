e5dd61539fe13ec2d7e94efdbca5e08a9dbb2f30
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-08-14 03:34:56 -0700

    Sortable widget list

--- web/components/dashboard/index.jsx
+++ web/components/dashboard/index.jsx
@@ -1,5 +1,7 @@
 import i18n from 'i18next';
 import React from 'react';
+import reactMixin from 'react-mixin';
+import Sortable from 'Sortable';
 import { Widget } from '../widget';
 
 class Widget1 extends Widget {
@@ -51,23 +53,41 @@ class Widget3 extends Widget {
     }
 }
 
+class WidgetList extends React.Component {
+    constructor(props) {
+        super(props);
+        this._sortableInstance = null;
+    }
+    componentDidMount() {
+        var dom = React.findDOMNode(this);
+        this._sortableInstance = Sortable.create(dom);
+    }
+    componentWillUnmount() {
+        this._sortableInstance.destroy();
+        this._sortableInstance = null;
+    }
+    render() {
+        return (
+            <div className="row">
+                <div className="col-sm-6">
+                    <Widget1/>
+                </div>
+                <div className="col-sm-6">
+                    <Widget2/>
+                </div>
+                <div className="col-sm-12">
+                    <Widget3/>
+                </div>
+            </div>
+        );
+    }
+}
+
 class Dashboard extends React.Component {
     render() {
         return (
             <div className="container no-heading">
-                <div className="row">
-                    <div className="col-sm-6">
-                        <Widget1/>
-                    </div>
-                    <div className="col-sm-6">
-                        <Widget2/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        <Widget3/>
-                    </div>
-                </div>
+                <WidgetList/>
             </div>
         );
     }
