41a79f031a53a53a8495bc38a447a4146c071e67
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-08-17 00:05:13 -0700

    Update components

--- web/components/home/index.jsx
+++ web/components/home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import i18n from 'i18next';
 
-class Home extends React.Component {
+export class Home extends React.Component {
     render() {
         return (
             <div>
@@ -9,7 +9,9 @@ class Home extends React.Component {
                     <div className="container">
                         <h1>{i18n._('Welcome to WebAppEngine!')}</h1>
                         <p>{i18n._('If you see this page, the WebAppEngine server is successfully installed and working. Further configuration is required.')}</p>
-                        <p dangerouslySetInnerHTML={{__html: i18n._('For online documentation please refer to <a href="http://cheton.github.io/webappengine/">http://cheton.github.io/webappengine/</a>.')}} />
+                        <p dangerouslySetInnerHTML={
+                            {__html: i18n._('For online documentation please refer to <a href="http://cheton.github.io/webappengine/">http://cheton.github.io/webappengine/</a>.')}
+                        } />
                         <p><a className="btn btn-primary btn-lg" href="https://github.com/cheton/webappengine" role="button">{i18n._('Learn more »')}</a></p>
                         <p><i>{i18n._('Thank you for using WebAppEngine.')}</i></p>
                     </div>
@@ -19,7 +21,3 @@ class Home extends React.Component {
         );
     }
 }
-
-module.exports = {
-    Home: Home
-};
