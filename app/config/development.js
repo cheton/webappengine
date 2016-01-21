1b580eb0f5ee0ea6982269a16c1d5be0ac5807a2
Cheton Wu <cheton@gmail.com>
2016-01-21 11:04:13 +0800

    Remove file logger

--- app/config/development.js
+++ app/config/development.js
@@ -26,26 +26,6 @@ var settings = {
                 timestamp: true, // or function()
                 json: false,
                 handleExceptions: true
-            },
-            File: {
-                level: 'trace',
-                silent: false,
-                colorize: false,
-                timestamp: true, // or function()
-                filename: 'log/app%s.log',
-                maxsize: 104857600,
-                maxFiles: 10,
-                json: true,
-                handleExceptions: true
-            }
-        },
-        exceptionHandlers: {
-            File: {
-                timestamp: true, // or function()
-                filename: 'log/error%s.log',
-                maxsize: 104857600,
-                maxFiles: 10,
-                json: true
             }
         }
     }
